f90c98abdeda3e06c79da82e1ade87cba1df593f
unknown <ricadocastellob@gmail.com>
2024-02-01 18:55:08 +0100

    fixMysql2 production

--- api/config/database.js
+++ api/config/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
+import mysql2 from 'mysql2';
 
 dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
+  dialectModule:mysql2,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
