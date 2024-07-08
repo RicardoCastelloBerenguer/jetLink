44af4a9ca9eac235cece34987d7fa344e6fb3463
RicardoCastelloBerenguer <ricardocastellob@gmail.com>
2024-07-08 12:43:01 +0200

    Fix pg compatibility

--- api/config/database.js
+++ api/config/database.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import mysql2 from "mysql2";
+import pg from "pg";
 
 dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: "postgres", // Supabase utiliza PostgreSQL
+  dialectModule: pg,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
