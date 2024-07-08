c6457fe29579607e51bc5abfe9a79e446b7d898f
RicardoCastelloBerenguer <ricardocastellob@gmail.com>
2024-07-08 12:09:43 +0200

    Migrate to Supabase 0.8

--- api/config/database.js
+++ api/config/database.js
@@ -1,16 +1,16 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv'
-import mysql2 from 'mysql2';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import mysql2 from "mysql2";
 
 dotenv.config();
 
 const sequelize = new Sequelize({
-  dialect: 'mysql',
-  dialectModule:mysql2,
+  dialect: "postgres", // Supabase utiliza PostgreSQL
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   dialectOptions: {
     ssl: {
       rejectUnauthorized: true,
--- api/index.js
+++ api/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import sequelize from './config/database.js';
-import Url from './models/Url.js';
-import User from './models/User.js';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import urlsRouter from './routes/urls.js';
-import userRouter from './routes/auth.js';
+import express from "express";
+import sequelize from "./config/database.js";
+import Url from "./models/Url.js";
+import User from "./models/User.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import urlsRouter from "./routes/urls.js";
+import userRouter from "./routes/auth.js";
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
 //SERVER SETUP
 
 const corsOptions = {
   // origin:process.env.BASE_URL_FRONT,
-    origin: 'https://jetlinkr.vercel.app',
-    optionsSuccessStatus: 200, // algunas versiones de los navegadores 204 
-  };
+  origin: "https://jetlinkr.vercel.app",
+  optionsSuccessStatus: 200, // algunas versiones de los navegadores 204
+};
 
 app.use(cors(corsOptions));
 
@@ -27,19 +27,20 @@ app.use(express.json());
 // Sincroniza el modelo con la base de datos
 (async () => {
   try {
+    await sequelize.authenticate();
+    console.log("Base de datos sincronizada correctamente");
+    // Sincronizar modelos y crear tablas si no existen
+
     await sequelize.sync({ force: false });
-    console.log('Base de datos sincronizada correctamente');
+    console.log("Modelos sincronizados correctamente con la base de datos.");
+
+    app.use("/", urlsRouter);
+    app.use("/auth", userRouter);
+
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port " + process.env.PORT);
+    });
   } catch (error) {
-    console.error('Error al sincronizar la base de datos:', error);
+    console.error("Error al sincronizar la base de datos:", error);
   }
 })();
-
-
-app.use('/', urlsRouter);
-app.use('/auth', userRouter);
-
-app.listen(process.env.PORT , () => {
-    console.log("listening on port " + process.env.PORT);
-});
-
-
